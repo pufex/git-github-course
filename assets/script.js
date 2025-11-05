be0ab8d8f63c3ab3cf6961607b307445f39367ef
Jakub Abram <j.abram1@wp.pl>
2025-11-05 21:51:02 +0100

    Added feedback modal to the contact form.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,25 @@
 // Some javascript changes to the file
+const modalContainer = document.querySelector(".contact-modal")
+const modalXIcon = document.querySelector(".contact-modal__close-icon");
+const contactForm = document.querySelector("#contact-form-id")
+const modalCloseBtn = document.querySelector(".contact-modal__close-btn")
+
+modalXIcon.addEventListener("click", () => {
+    if(!modalContainer.classList.contains("hidden")){
+        modalContainer.classList.add("hidden")
+    }
+})
+
+modalCloseBtn.addEventListener("click", () => {
+    if(!modalContainer.classList.contains("hidden")){
+        modalContainer.classList.add("hidden")
+    }
+})
+
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(modalContainer.classList.contains("hidden")){
+        modalContainer.classList.remove("hidden")
+    }
+})
+
